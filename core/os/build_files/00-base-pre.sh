57526d9d61edb08277c9bc3029a16753644e95bb
alex <alex@pibloom.com>
2026-03-16 22:52:10 +0200

    chore: delete Fedora bootc Containerfile, BIB, build scripts, system_files, disk_config

--- core/os/build_files/00-base-pre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-# Remove packages that conflict with bootc immutability or are unnecessary
-grep -vE '^\s*(#|$)' /ctx/packages/packages-remove.txt | xargs dnf -y remove || true
-dnf -y autoremove || true
--- core/os/system_files/etc/skel/.bashrc
+++
@@ -1,4 +0,0 @@
-export BLOOM_DIR="$HOME/Bloom"
-export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-export BROWSER="chromium"
-export PATH="/usr/local/share/bloom/node_modules/.bin:$PATH"
